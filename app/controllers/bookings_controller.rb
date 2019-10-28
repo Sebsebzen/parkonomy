9362a47baef04c14136c27d5d29268bb8b00e800
sisi florensia <sisiflorensia@gmail.com>
2019-10-28 18:36:07 +0800

    Add placeholder methods for carparks and bookings controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,51 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(
+      :user_id,
+      :carpark_id,
+      :start_date_time,
+      :end_date_time,
+      :total_price
+    )
+  end
 end
--- app/controllers/carparks_controller.rb
+++ app/controllers/carparks_controller.rb
@@ -1,2 +1,55 @@
 class CarparksController < ApplicationController
+  before_action :set_carpark, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @carparks = Carpark.all
+  end
+
+  def show
+  end
+
+  def new
+    @carpark = Carpark.new
+  end
+
+  def create
+    @carpark = Carpark.new(carpark_params)
+    if @carpark.save
+      redirect_to new_carpark_path(@carpark)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @carpark.update(carpark_params)
+  end
+
+  def destroy
+    @carpark.destroy
+    redirect_to carparks_path
+  end
+
+  private
+
+  def set_carpark
+    @carpark = Carpark.find(params[:id])
+  end
+
+  def carpark_params
+    params.require(:carpark).permit(
+      :user_id,
+      :average_rating,
+      :length,
+      :width,
+      :height,
+      :latitude,
+      :longitude,
+      :rate_hour,
+      :photo
+    )
+  end
 end
