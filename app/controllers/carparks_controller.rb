935969d5ee790bd4bdefcddb1fe971edbf37fda8
sisi florensia <sisiflorensia@gmail.com>
2019-10-29 18:20:16 +0800

    Bookings controllers fixed. User authentications fixed

--- app/controllers/carparks_controller.rb
+++ app/controllers/carparks_controller.rb
@@ -1,5 +1,6 @@
 class CarparksController < ApplicationController
   before_action :set_carpark, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show, :search]
 
   def index
     @carparks = Carpark.all
