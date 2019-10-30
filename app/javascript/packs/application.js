1bc39bce7cdc71754f543dc065da078b64bb8b50
Sebastian Bausch <sebasbeijing@gmail.com>
2019-10-30 16:31:14 +0800

    Fix CSS for MapBox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@ import "bootstrap";
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initMapbox } from '../plugins/init_mapbox';
 
-import 'mapbox-gl/dist/mapbox-gl.css';
 
 initAutocomplete();
 initMapbox();
