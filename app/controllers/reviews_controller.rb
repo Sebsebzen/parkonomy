157d3fd4b8f8081c99bbdb68d7519f4d5f73ee8f
Ryan Ashton <ryan.mark.ashton@gmail.com>
2019-10-31 12:52:04 +0800

    updated reviews on carparks/show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
+  end
+
 end
