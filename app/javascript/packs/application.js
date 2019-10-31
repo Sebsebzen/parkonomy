368b9593481cd895b8395fbcddf6952aa88cf3dc
sisi florensia <sisiflorensia@gmail.com>
2019-10-31 12:04:11 +0800

    Flatpickr fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,14 @@
 import "bootstrap";
+import flatpickr from "flatpickr";
+
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initMapbox } from '../plugins/init_mapbox';
 import { loadDynamicBannerText } from '../components/banner';
-import flatpickr from "flatpickr";
-
+import { initFlatpickr } from '../plugins/init_flatpickr';
 
 initUpdateNavbarOnScroll();
 initAutocomplete();
 initMapbox();
 loadDynamicBannerText();
+initFlatpickr();
