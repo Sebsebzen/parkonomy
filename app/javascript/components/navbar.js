c937cb805886f3c30763ae4442fb16363c154c1e
sisi florensia <sisiflorensia@gmail.com>
2019-11-01 11:59:02 +0800

    Add banner to dashboard and some little details

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -4,11 +4,23 @@ const initUpdateNavbarOnScroll = () => {
   const navLink = document.querySelectorAll('.nav-link');
   // console.log(navLink)
 
-  if (window.location.pathname == '/') {
+  if (window.location.pathname == '/' || window.location.pathname == '/bookings') {
     navLink.forEach((item) => {
-      item.classList.add('text-white');
-    });
-  } else if (window.location.pathname.includes('/search')) {
+        item.classList.add('text-white');
+    });window.addEventListener('scroll', () => {
+    if (window.scrollY >= window.innerHeight*0.1) {
+      navbar.classList.add('navbar-lewagon-white');
+      navLink.forEach((item) => {
+        item.classList.remove('text-white');
+      });
+    } else {
+      navbar.classList.remove('navbar-lewagon-white');
+      navLink.forEach((item) => {
+        item.classList.add('text-white');
+      });
+    }
+  });
+  } else {
     navbar.classList.add('navbar-lewagon-opaque');
     window.addEventListener('scroll', () => {
     if (window.scrollY >= window.innerHeight*0.1) {
@@ -16,14 +28,6 @@ const initUpdateNavbarOnScroll = () => {
       navbar.classList.add('navbar-lewagon-white');
     } else {
       navbar.classList.add('navbar-lewagon-opaque')
-      navbar.classList.remove('navbar-lewagon-white');
-    }
-  });
-  } else if (navbar) {
-    window.addEventListener('scroll', () => {
-    if (window.scrollY >= window.innerHeight*0.1) {
-      navbar.classList.add('navbar-lewagon-white');
-    } else {
       navbar.classList.remove('navbar-lewagon-white');
     }
   });
