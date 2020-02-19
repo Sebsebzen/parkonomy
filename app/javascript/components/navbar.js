c251b081a6607510f5b54c691ce82839108701f9
sisi florensia <sisiflorensia@gmail.com>
2020-02-19 20:44:53 +0800

    restore red icon & smooth navbar transitions

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -8,22 +8,23 @@ const initUpdateNavbarOnScroll = () => {
     navLink.forEach((item) => {
         item.classList.add('text-white');
     });window.addEventListener('scroll', () => {
-    if (window.scrollY >= window.innerHeight*0.1) {
+    if (window.scrollY >= window.innerHeight*0.05) {
       navbar.classList.add('navbar-lewagon-white');
-      navLink.forEach((item) => {
-        item.classList.remove('text-white');
-      });
+      // navLink.forEach((item) => {
+      //   console.log(item);
+      //   item.classList.remove('text-white');
+      // });
     } else {
       navbar.classList.remove('navbar-lewagon-white');
-      navLink.forEach((item) => {
-        item.classList.add('text-white');
-      });
+      // navLink.forEach((item) => {
+      //   item.classList.add('text-white');
+      // });
     }
   });
   } else {
     navbar.classList.add('navbar-lewagon-opaque');
     window.addEventListener('scroll', () => {
-    if (window.scrollY >= window.innerHeight*0.1) {
+    if (window.scrollY >= window.innerHeight*0.05) {
       navbar.classList.remove('navbar-lewagon-opaque')
       navbar.classList.add('navbar-lewagon-white');
     } else {
